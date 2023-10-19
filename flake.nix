337280531cfe3d22bdc22c9e6f0b9d90b7a5550f
rjpc <rjpc@rjpc.net>
2023-10-18 20:19:48 -0700

    nixpkgs-fmt and remove comments

--- flake.nix
+++ flake.nix
@@ -13,14 +13,11 @@
     let
       system = "aarch64-darwin";
       pkgs = nixpkgs.legacyPackages.${system};
-    in {
+    in
+    {
       homeConfigurations."rjpc" = home-manager.lib.homeManagerConfiguration {
         inherit pkgs;
-
-        # Specify your home configuration modules here, for example,
-        # the path to your home.nix.
         modules = [ ./home.nix ];
-
         # Optionally use extraSpecialArgs
         # to pass through arguments to home.nix
       };
